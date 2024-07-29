b0388fe734d567c3ff91817f15fe40c9c692ea54
Yoimar77 <yoimarnova123@gmail.com>
2024-07-29 07:47:49 -0500

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-
-public class main {
+public class Main {
     public static void mostrarMenuPrincipal() {
         System.out.println("----- MENÚ PRINCIPAL -----");
         System.out.println("1. Casteo");
@@ -25,8 +24,9 @@ public class main {
         System.out.println("11. Volver al menú principal");
         System.out.print("Selecciona una opción: ");
     }
+
     public static void mostrarSubmenuAsignacion() {
-        System.out.println("----- CASTEO -----");
+        System.out.println("----- OPERADORES DE ASIGNACION -----");
         System.out.println("1. Ejercicio");
         System.out.println("2. Ejercicio");
         System.out.println("3. Ejercicio");
@@ -37,7 +37,7 @@ public class main {
     }
 
     public static void mostrarSubmenuID() {
-        System.out.println("----- CASTEO -----");
+        System.out.println("----- OPERADORES DE INCREMENTO Y DECREMENTO -----");
         System.out.println("1. Ejercicio");
         System.out.println("2. Ejercicio");
         System.out.println("3. Ejercicio");
@@ -48,7 +48,7 @@ public class main {
     }
 
     public static void mostrarSubmenuCombinados() {
-        System.out.println("----- CASTEO -----");
+        System.out.println("----- EJERCICIOS COMBINADOS -----");
         System.out.println("1. Ejercicio");
         System.out.println("2. Ejercicio");
         System.out.println("3. Ejercicio");
@@ -62,252 +62,198 @@ public class main {
         System.out.println("11. Volver al menú principal");
         System.out.print("Selecciona una opción: ");
     }
-}
-// CASTEO
-// 1.¿Cuál será el resultado de la siguiente operación?
-public static void casteo1() {
-    int x = (int) 5.89 + (int) 3.45;
-    System.out.println("El resultado es: " + x);
-}
+    //
 
-
-//2.  Dado char c = 'A';, ¿cómo convertirías c a su valor ASCII como un int?
-public static void casteo2() {
-    char myChar = 'A';
-    int myInt = (int) myChar;
-    System.out.println(myInt);
-}
-// El resultado sera 65 (ASCII de 'A')
-
-
-// 3. ¿Qué valor se imprimirá?
-public static void casteo3() {
-    double d = 100.04;
-    long l = (long) d;
-    int i = (int) l;
-    System.out.println(i);
-}
-// El resultado es 100
-
-// 4. Escribe código para convertir un String que contiene un número a un int.
-public static void casteo4() {
-    char myChar = 'E';
-    int myInt = (int) myChar;
-    System.out.println(myInt);
-}
-// El resultado es 69
-
-// 5. ¿Cuál es el resultado de (int) (char) (byte) -1?
-public static void casteo5(){
-    int result = (int) (char) (byte) -1;
-    System.out.println("Resultado: " + result);
-    {
-        //El resultado es 65535
-
-        // 6. Convierte el número 65 a un char. ¿Qué carácter obtienes
-        public static void casteo6() {
-        int myInt = 65;
-        char myChar = (char) myInt;
-        System.out.println(myChar);
+    public static void casteoUno(){
+        int x = (int) 5.89 + (int) 3.45;
+        System.out.println("El resultado es: " +x);
     }
-        // El resultado es A
 
-        // 7. ¿Qué sucede si intentas asignar un long a un int sin casteo explícito?
-        public static void casteo7() {
-        long c = 180;
-        int z = (int) c;
-        System.out.println(z);
+    public static void casteoDos(){
+        char c = 'A';
+        int n = (int) c;
+        System.out.println("El resultado es: " +n);
     }
-        //El resultado es 180
 
-        //8. Escribe código para convertir un float a un String.
-        public static void casteo8() {
+    public static void casteoTres(){
+        double d = 100.04;
+        long l = (long)d;
+        int i = (int)l;
+        System.out.println("El resultado es: " +i);
+    }
+
+    public static void casteoCuatro(){
+        char myChar = 'E';
+        int myInt = (int) myChar;
+        System.out.println("El resultado es: " +myInt);
+    }
+
+    public static void casteoCinco(){
+        int result =  (char) (byte) -1;
+        System.out.println("Resultado: " + result);
+    }
+
+    public static void casteoSeis(){
+        int numero = 65;
+        char caracter = (char) numero;
+        System.out.println("El resultado es: " +caracter);
+    }
+
+    public static void casteoSiete(){
+        long s = 180;
+        int h = (int)s;
+        System.out.println("El resultado es: " +h);
+    }
+
+    public static void casteoOcho(){
         float v = 17;
-        String m = "" + v;
-        System.out.println(m);
+        String m ="" + v;
+        System.out.println("El resultado es: " +m);
     }
-        // El resultado es 17.0
 
-        // 9. ¿Cuál es el resultado de (int) Math.round(3.7)?
-        public static void casteo9() {
+    public static void casteoNueve(){
         int resultado = (int) Math.round(3.7);
-        System.out.println(resultado);
-    }// El resultado es 4
+        System.out.println("El resultado es: " +resultado);
+    }
 
-        //10. Dado byte b = 50;, ¿cómo lo convertirías a short?
-        public static void casteo10() {
+    public static void casteoDiez(){
         byte b = 50;
-        short s = (short) b;
-        System.out.println(s);
-    } // El resultado es 50
-// FIN CASTEO
-
-        //OPERADORES DE ASIGNACION
-        // 1. ¿Cuál es el valor de x después de la siguiente operación?
-        //int x = 10; x += 5;
-        public static void asignacion1() {
+        short w = (short) b;
+        System.out.println("El resultado es: " +w);
+    }
+    //
+    public static void asignacionUno(){
         int x = 10;
         x += 5;
-        System.out.println(x);
-    } // El resultado es 15
+        System.out.println("El resultado es: " +x);
+    }
 
-        // 2. Simplifica la siguiente expresión usando un operador de asignación compuesta:
-        //y = y * (x + 5);
+    public static void asignacionDos(){
+        int s = 2;
+        int y = 25;
+        y *= (s + 5);
+        System.out.println("El resultado es: " +y);
+    }
 
-        public static void asignacion2() {
-        int x = 0;
-        int y = 20;
-        y *= (x + 5);
-        System.out.println(y);
-    }//El resultado es 100
-
-        // 3. ¿Cuál es el valor de a después de estas operaciones?
-        //int a = 15; a %= 4;
-        public static void asignacion3() {
+    public static void asignacionTres(){
         int a = 15;
         a %= 4;
-        System.out.println(a);
-    } // El resultado es 3
+        System.out.println("El resultado es: " +a);
+    }
 
-        // 4. Escribe una expresión equivalente a x = x + y usando un operador de asignación compuesta.
-        public static void asignacion4() {
-        int x = 4;
-        int y = 2;
-        x += y;
-        System.out.println(x);
-    }// El resultado es 6
+    public static void asignacionCuatro(){
+        int h = 19;
+        int b = 17;
+        h+=b;
+        System.out.println("El resultado es: " +h);
+    }
 
-        // 5. Qué hace la siguiente operación? x ^= 2;
-        public static void asignacion5() {
-        int x = 1;
-        x ^= 2;
-        System.out.println(x);
-    } // El resultado es 3
+    public static void asignacionCinco(){
+        int m =5;
+        m^=2;
+        System.out.println("El resultado es: " +m);
+    }
 
-        //FIN OPERACIONES ASIGNACION
-
-
-        //OPERADORES DE INCREMENTO Y DECREMENTO
-
-        //1. ¿Cuál es el valor de x e y después de estas operaciones?
-        //int x = 5; int y = ++x;
-
-        public static void incrementoDecremento1() {
+    //
+    public static void IDuno(){
         int x = 5;
-        System.out.println(x);
         int y = ++x;
+        System.out.println(x);
         System.out.println(y);
-    }//El resultado es 5 y 6
+    }
 
-        // 2. ¿Qué se imprimirá en la consola?
-        //int a = 5; System.out.println(a++); System.out.println(a);
-
-        public static void incrementoDecremento2() {
+    public static void IDdos(){
         int a = 5;
         System.out.println(a++);
         System.out.println(a);
-    }// El resultado es 5 y 6
+    }
 
-        // 3.scribe código que use el operador de decremento para disminuir una variable count en 1.
-        public static void incrementoDecremento3() {
-        int count = 20;
+    public static void IDtres(){
+        int count = 3;
         count--;
-        System.out.println(count);
-    }  //El resultado es 19
+        System.out.println("El resultado es: " +count);
+    }
 
-        // 4. ¿Cuál es la diferencia entre ++i y i++?
-        public static void incrementoDecremento4() {
-        int i = 5;
-        i++;
-        System.out.println(i);
-    }// El resultado es 6
+    public static void IDcuatro(){
+        int i = 7;
+        ++i;
+        System.out.println("Operacion antes de usar el valor"+i);
+        int i2 = 7;
+        i2++;
+        System.out.println("Operacion despues de usar el valor"+i2);
+    }
 
-        // 5. ¿Qué valor tendrá x después de esta operación?
-        public static void incrementoDecremento5() {
-        int x = 3;
-        x++;
-        System.out.println(x);
-    }//El resultado es 4
-        //FIN OPERADORES DE INCREMENTO Y DECREMENTO
+    public static void IDcinco(){
+        int xf = 3;
+        xf++;
+        System.out.println("El resultado es: " +xf);
+    }
 
-        //EJERCICIOS COMBINADOS
-
-        //Ejercicio 1
-        public static void combinados1() {
+    //
+    public static void combinadosUno(){
         int i = 5;
         i += ++i + i++ + ++i;
-        System.out.println(i);
-    }// El resultado es 25
+        System.out.println("El resultado es: " +i);
+    }
 
-        //Ejercicio 2
-        public static void combinados2() {
-        double myDouble = 6.5;
+    public static void combinadosDos(){
+        double myDouble = 1.9;
         int myInt = (int) myDouble;
         System.out.println(myInt);
         System.out.println(++myInt);
-    }//El resultado es 6 y 7
+    }
 
-
-        // Ejercicio 3
-        public static void combinados3() {
+    public static void combinadosTres(){
         double d = 5.7;
-        int i = (int) d;
+        int i = (int)d;
         i *= 2;
         System.out.println(i--);
-    }//El resultado es 10
+    }
 
-        // Ejercicio 4
-        public static void combinados4() {
+    public static void combinadosCuatro(){
         int i = 10;
         double d = i;
         d /= 2.0;
-        System.out.println(d);
-    } //El resultado es 5.0
+        System.out.println("El resultado es: " +d);
+    }
 
-        //Ejercicio 5
-        public static void combinados5() {
-            char c = 'X';
+    public static void combinadosCinco(){
+        char c = 'X';
         c += 32;
-        System.out.println(c);
-        } //El resultado es X
+        System.out.println("El resultado es: " +c);
+    }
 
-        //Ejercicio 6
-        public static void combinados6() {
+    public static void combinadosSeis(){
         long l = 12345L;
         short s = (short) l;
-        s *= 3;
-        System.out.println(s);
-    }// El resultado es -28501
+        s*= 3;
+        System.out.println("El resultado es: " +s);
+    }
 
-        //Ejercicio 7
-        public static void combinados7() {
+    public static void combinadosSiete(){
         int x = 10;
         x += (x++) + (++x);
         System.out.println("El resultado es: " + x);
-    }//El resultado es 32
+    }
 
-        //Ejercicio 8
-        public static void combinados8() {
+    public static void combinadosOcho(){
         float f = 10.5f;
         byte b = (byte) f;
         b = (byte) (--b);
         System.out.println("El resultado es: " + b);
-    } //El resultado es 9
+    }
 
-        //Ejercicio 9
-        public static void combinados9() {
+    public static void combinadosNueve(){
         int i = 257;
         byte b = (byte) i;
         System.out.println("El resultado es: " + b);
-    } //El resultado es 1
+    }
 
-        //Ejercicio 10
-        public static void combinados10() { double d = 7.8;
+    public static void combinadosDiez(){
+        double d = 7.8;
         int i = (int) d;
         i += 5;
         i++;
-        System.out.println("El resultado es: " + i);}
-        //El resultado es 13
+        System.out.println("El resultado es: " + i);
     }
-
 }
--- src/solve.java
+++ src/solve.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-
 public class solve {
+    public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         int opcionMenu = 0;
         int opcionSubmenu = 0;
         boolean salir = false;
 
         while (!salir) {
-            main.mostrarMenuPrincipal();
+            Main.mostrarMenuPrincipal();
             opcionMenu = scanner.nextInt();
             boolean salirSubmenu = false;
 
@@ -17,49 +17,49 @@ public class solve {
 
 
                     while (!salirSubmenu) {
-                        main.mostrarSubmenuCasteo();
+                        Main.mostrarSubmenuCasteo();
                         opcionSubmenu = scanner.nextInt();
 
                         switch (opcionSubmenu) {
                             case 1:
                                 // ¿Cuál será el resultado de la siguiente operación?
-                                solve.casteo1;
+                                Main.casteoUno();
                                 break;
                             case 2:
                                 //2.  Dado char c = 'A';, ¿cómo convertirías c a su valor ASCII como un int?
-                                casteo2();
+                                Main.casteoDos();
                                 break;
                             case 3:
                                 // 3. ¿Qué valor se imprimirá?
-                                casteo3();
+                                Main.casteoTres();
                                 break;
                             case 4:
                                 // 4. Escribe código para convertir un String que contiene un número a un int.
-                                casteo4();
+                                Main.casteoCuatro();
                                 break;
                             case 5:
                                 // 5. ¿Cuál es el resultado de (int) (char) (byte) -1?
-                                casteo5();
+                                Main.casteoCinco();
                                 break;
                             case 6:
                                 // 6. Convierte el número 65 a un char. ¿Qué carácter obtienes?
-                                casteo6();
+                                Main.casteoSeis();
                                 break;
                             case 7:
                                 // 7. ¿Qué sucede si intentas asignar un long a un int sin casteo explícito?
-                                casteo7();
+                                Main.casteoSiete();
                                 break;
                             case 8:
                                 //8. Escribe código para convertir un float a un String.
-                                casteo8();
+                                Main.casteoOcho();
                                 break;
                             case 9:
                                 // 9. ¿Cuál es el resultado de (int) Math.round(3.7)?
-                                casteo9();
+                                Main.casteoNueve();
                                 break;
                             case 10:
                                 //10. Dado byte b = 50;, ¿cómo lo convertirías a short?
-                                casteo10();
+                                Main.casteoDiez();
                                 break;
                             case 11:
                                 System.out.println("Volviendo al menú principal.");
@@ -75,43 +75,31 @@ public class solve {
                     System.out.println("2. Operadores de Asignacion");
 
                     while (!salirSubmenu) {
-                        mostrarSubmenuAsignacion();
+                        Main.mostrarSubmenuAsignacion();
                         opcionSubmenu = scanner.nextInt();
 
                         switch (opcionSubmenu) {
                             case 1:
                                 // 1. ¿Cuál es el valor de x después de la siguiente operación?
-                                int x = 10;
-                                x += 5;
-                                System.out.println(x);
+                                Main.asignacionUno();
                                 break;
                             case 2:
                                 // 2. Simplifica la siguiente expresión usando un operador de asignación compuesta:
                                 //y = y (s + 5);
-                                int s = 2;
-                                int y = 25;
-                                y *= (s + 5);
-                                System.out.println(y);
+                                Main.asignacionDos();
                                 break;
                             case 3:
                                 // 3. ¿Cuál es el valor de a después de estas operaciones?
                                 //int a = 15; a %= 4;
-                                int a = 15;
-                                a %= 4;
-                                System.out.println(a);
+                                Main.asignacionTres();
                                 break;
                             case 4:
                                 // 4. Escribe una expresión equivalente a x = x + y usando un operador de asignación compuesta.
-                                int h = 19;
-                                int b = 17;
-                                h+=b;
-                                System.out.println(h);
+                                Main.asignacionCuatro();
                                 break;
                             case 5:
                                 // 5. Qué hace la siguiente operación? x ^= 2;
-                                int m =5;
-                                m^=2;
-                                System.out.println(m);
+                                Main.asignacionCinco();
                                 break;
                             case 6:
                                 System.out.println("Volviendo al menú principal.");
@@ -127,45 +115,31 @@ public class solve {
                     System.out.println("3. Operadores de Incremento y Decremento");
 
                     while (!salirSubmenu) {
-                        mostrarSubmenuID();
+                        Main.mostrarSubmenuID();
                         opcionSubmenu = scanner.nextInt();
 
                         switch (opcionSubmenu) {
                             case 1:
                                 //1. ¿Cuál es el valor de x e y después de estas operaciones?
                                 //int x = 5; int y = ++x;
-                                int x = 5;
-                                int y = ++x;
-                                System.out.println(x);
-                                System.out.println(y);
+                                Main.IDuno();
                                 break;
                             case 2:
                                 // 2. ¿Qué se imprimirá en la consola?
                                 //int a = 5; System.out.println(a++); System.out.println(a);
-                                int a = 5;
-                                System.out.println(a++);
-                                System.out.println(a);
+                                Main.IDdos();
                                 break;
                             case 3:
                                 // 3. Escribe código que use el operador de decremento para disminuir una variable count en 1.
-                                int count = 3;
-                                count--;
-                                System.out.println(count);
+                                Main.IDtres();
                                 break;
                             case 4:
                                 // 4. ¿Cuál es la diferencia entre ++i y i++?
-                                int i = 7;
-                                ++i;
-                                System.out.println("Operacion antes de usar el valor"+i);
-                                int i2 = 7;
-                                i2++;
-                                System.out.println("Operacion despues de usar el valor"+i2);
+                                Main.IDcuatro();
                                 break;
                             case 5:
                                 // 5. ¿Qué valor tendrá xf después de esta operación?
-                                int xf = 3;
-                                xf++;
-                                System.out.println(xf);
+                                Main.IDcinco();
                                 break;
                             case 6:
                                 System.out.println("Volviendo al menú principal.");
@@ -180,51 +154,39 @@ public class solve {
                 case 4:
                     System.out.println("4. Ejercicios Combinados");
                     while (!salirSubmenu) {
-                        mostrarSubmenuCombinados();
+                        Main.mostrarSubmenuCombinados();
                         opcionSubmenu = scanner.nextInt();
 
                         switch (opcionSubmenu) {
                             case 1:
-                                // ¿Cuál será el resultado de la siguiente operación?
-                                int x = (int) 5.89 + (int) 3.45;
-                                System.out.println(x);
-                                // El resultado es 8
+                                Main.combinadosUno();
                                 break;
                             case 2:
-                                System.out.println("Realizando operación de resta...");
-                                // Aquí puedes implementar la lógica para la resta
+                                Main.combinadosDos();
                                 break;
                             case 3:
-                                System.out.println("Realizando operación de resta...");
-                                // Aquí puedes implementar la lógica para la resta
+                                Main.combinadosTres();
                                 break;
                             case 4:
-                                System.out.println("Realizando operación de resta...");
-                                // Aquí puedes implementar la lógica para la resta
+                                Main.combinadosCuatro();
                                 break;
                             case 5:
-                                System.out.println("Realizando operación de resta...");
-                                // Aquí puedes implementar la lógica para la resta
+                                Main.combinadosCinco();
                                 break;
                             case 6:
-                                System.out.println("Realizando operación de resta...");
-                                // Aquí puedes implementar la lógica para la resta
+                                Main.combinadosSeis();
                                 break;
                             case 7:
-                                System.out.println("Realizando operación de resta...");
-                                // Aquí puedes implementar la lógica para la resta
+                                Main.combinadosSiete();
                                 break;
                             case 8:
-                                System.out.println("Realizando operación de resta...");
-                                // Aquí puedes implementar la lógica para la resta
+                                Main.combinadosOcho();
                                 break;
                             case 9:
-                                System.out.println("Realizando operación de resta...");
-                                // Aquí puedes implementar la lógica para la resta
+                                Main.combinadosNueve();
                                 break;
                             case 10:
-                                System.out.println("Realizando operación de resta...");
-                                // Aquí puedes implementar la lógica para la resta
+                                Main.combinadosDiez();
                                 break;
                             case 11:
                                 System.out.println("Volviendo al menú principal.");
@@ -246,4 +208,5 @@ public class solve {
             }
         }
         scanner.close();
+    }
 }
